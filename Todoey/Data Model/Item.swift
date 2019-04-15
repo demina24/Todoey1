b458ff1c09c9f208722ba173771ab0ec93eec0fd
Dimitra Stefanidou <dimitrastefanidou@dimitras-macbook-pro.local>
2019-04-15 15:45:25 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dimitra Stefanidou on 4/12/19.
+//  Copyright © 2019 Dimitra Stefanidou. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
